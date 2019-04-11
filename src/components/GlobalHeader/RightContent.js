18f1f1456f7f37dc19bd3e15e8d3cf250b46c6f5
liu.yalei <liu.yalei@mwee.cn>
2019-04-11 12:38:46 +0800

    update:umi(#61)

--- src/components/GlobalHeader/RightContent.js
+++ src/components/GlobalHeader/RightContent.js
@@ -78,7 +78,7 @@ export default class GlobalHeaderRight extends PureComponent {
   };
 
   render() {
-    const {
+    let {
       currentUser,
       fetchingMoreNotices,
       fetchingNotices,
@@ -89,6 +89,56 @@ export default class GlobalHeaderRight extends PureComponent {
       skeletonCount,
       theme,
     } = this.props;
+    currentUser = {
+      name: 'Mr.liu1',
+        avatar: 'https://gw.alipayobjects.com/zos/rmsportal/BiazfanxmamNRoxxVxka.png',
+      userid: '00000001',
+      email: 'antdesign@alipay.com',
+      signature: '海纳百川，有容乃大',
+      title: '交互专家',
+      group: '蚂蚁金服－某某某事业群－某某平台部－某某技术部－UED',
+      tags: [
+      {
+        key: '0',
+        label: '很有想法的',
+      },
+      {
+        key: '1',
+        label: '专注设计',
+      },
+      {
+        key: '2',
+        label: '辣~',
+      },
+      {
+        key: '3',
+        label: '大长腿',
+      },
+      {
+        key: '4',
+        label: '川妹子',
+      },
+      {
+        key: '5',
+        label: '海纳百川',
+      },
+    ],
+      notifyCount: 12,
+      unreadCount: 11,
+      country: 'China',
+      geographic: {
+      province: {
+        label: '浙江省',
+          key: '330000',
+      },
+      city: {
+        label: '杭州市',
+          key: '330100',
+      },
+    },
+      address: '西湖区工专路 77 号',
+        phone: '0752-268888888',
+    }
     const menu = (
       <Menu className={styles.menu} selectedKeys={[]} onClick={onMenuClick}>
         <Menu.Item key="userCenter">
