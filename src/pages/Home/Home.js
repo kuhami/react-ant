26bcf317df493f5fe6c6eec172a1e6c5b03903e8
liu.yalei <liu.yalei@mwee.cn>
2019-05-09 10:30:55 +0800

    update:home (#87)

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,24 +1,47 @@
 import React,{Component} from 'react';
 import { Button  } from 'antd';
-
+import Link from 'umi/link';
+import tabs from './home.json'
 export default class Home extends Component {
   constructor(props){
     super(props);
     let userName = localStorage.getItem('antd-pro-authority')
-    console.log(JSON.parse(userName));
+    console.log(userName);
+    this.state = {
+      tabs
+    }
   }
   componentDidMount() {
 
   }
-  onHandlePage = ()=>{
-    const {onHandlePage} = this.props.location
-    onHandlePage({key:'/study/Components'});
+  onHandlePage = (e,val)=>{
+    const {key} = val,{onHandlePage} = this.props.location;
+    onHandlePage({key});
   }
   render() {
-
+    console.log(this.state.tabs);
     return (
-      <div style={{width:'100%',textAlign:'center',marginTop:'40px'}}>
-        <h2 style={{color:'rgb(144, 159, 172)',fontFamily:'cursive',fontSize:'42px',fontWeight:'700'}}>欢迎使用多标签Ant-Tabs</h2>
+      <div style={{width:'100%',height:'calc(100vh - 64px)',textAlign:'center',background:'#fff'}}>
+        <div style={{userSelect:'none'}}>
+          <h2 style={{color:'rgb(144, 159, 172)',fontFamily:'cursive',fontSize:'42px',fontWeight:'700',padding:"40px "}}>欢迎使用多标签Ant-Tabs</h2>
+          <div className="myHome" >
+            <ul >
+              {
+                this.state.tabs.map((val)=>{
+                  return(<li key={"key_" + val.fd_fsid}
+                             onClick={(e)=>this.onHandlePage(e,val)}>
+                    <div><img  src={val.IconUrl} alt=""/></div>
+                    <p>{val.Name}</p>
+                  </li>)
+                })
+              }
+              {/*<li onClick={this.handleAdd}>*/}
+                {/*<img style={{width:112,background:'#d4c49b',borderRadius:'50%'}} src={''} alt="1"/>*/}
+                {/*<p>编辑模块</p>*/}
+              {/*</li>*/}
+            </ul>
+          </div>
+        </div>
       </div>
     );
   }
