3bcee0a6d7e9ccc03c93375c5d88f7b8d84b8797
liu.yalei <liu.yalei@mwee.cn>
2019-06-27 16:18:27 +0800

    change:email my (#107)

--- mock/user.js
+++ mock/user.js
@@ -5,7 +5,7 @@ export default {
     name: 'Mr.liu',
     avatar: 'https://gw.alipayobjects.com/zos/rmsportal/BiazfanxmamNRoxxVxka.png',
     userid: '00000001',
-    email: 'antdesign@alipay.com',
+    email: 'liuyalei218@gmail.com',
     signature: '海纳百川，有容乃大',
     title: '交互专家',
     group: '蚂蚁金服－某某某事业群－某某平台部－某某技术部－UED',
--- src/components/GlobalHeader/RightContent.js
+++ src/components/GlobalHeader/RightContent.js
@@ -93,7 +93,7 @@ export default class GlobalHeaderRight extends PureComponent {
       name: 'Mr.liu',
         avatar: 'https://gw.alipayobjects.com/zos/rmsportal/BiazfanxmamNRoxxVxka.png',
       userid: '00000001',
-      email: 'antdesign@alipay.com',
+      email: 'liuyalei218@gmail.com',
       signature: '海纳百川，有容乃大',
       title: '交互专家',
       group: '蚂蚁金服－某某某事业群－某某平台部－某某技术部－UED',
